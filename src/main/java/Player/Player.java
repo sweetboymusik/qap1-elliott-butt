92f6a6113527f8dc9f151c6f806d23927b72bc29
sweetboymusik <ellbutt@gmail.com>
2024-10-02 13:09:15 -0230

    added Player class

---
+++ src/main/java/Player/Player.java
@@ -0,0 +1,58 @@
+package Player;
+
+public class Player {
+    // instance variables
+    private final int maxHealth;
+    private int health;
+    private int strength;
+
+    // constructors
+    public Player(int maxHealth, int strength) {
+        this.maxHealth = maxHealth;
+        this.health = maxHealth;
+        this.strength = strength;
+    }
+
+    // getters and setters
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    // methods
+    public void attack() {
+    }
+
+    public void takeDamage(int amount) {
+        int newHealth = health - amount;
+
+        if (newHealth < 0)
+            newHealth = 0;
+
+        setHealth(newHealth);
+    }
+
+    public void heal(int amount) {
+        int newHealth = health + amount;
+
+        if (newHealth > maxHealth)
+            newHealth = maxHealth;
+
+        setHealth(newHealth);
+    }
+}
