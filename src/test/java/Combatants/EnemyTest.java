fa2e08433b420a6e852a12f5af35f77ffb91a8b3
sweetboymusik <ellbutt@gmail.com>
2024-10-03 20:56:24 -0230

    set up test files for enemy and player classes

---
+++ src/test/java/Combatants/EnemyTest.java
@@ -0,0 +1,12 @@
+package Combatants;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EnemyTest {
+
+    @Test
+    void chooseAction() {
+    }
+}
---
+++ src/test/java/Combatants/PlayerTest.java
@@ -0,0 +1,20 @@
+package Combatants;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @Test
+    void gainExp() {
+    }
+
+    @Test
+    void levelUp() {
+    }
+
+    @Test
+    void rollStat() {
+    }
+}
